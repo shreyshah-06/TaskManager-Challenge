1510ed40d17e1bf2914d7678682e9f0b7d26c4ea
shreyshah-06 <shrey.shah0610@gmail.com>
2024-12-01 00:18:42 +0530

    bug fix

--- src/App.js
+++ src/App.js
@@ -8,26 +8,27 @@ import TokenPage from './pages/TokenPage';
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  useEffect(() => {
-    // Check if the auth token exists in localStorage
-    const token = localStorage.getItem('authToken');
-    if (token) {
-      setIsAuthenticated(true);
-    } else {
-      setIsAuthenticated(false);
-    }
-  }, [isAuthenticated]);
+  // useEffect(() => {
+  //   // Check if the auth token exists in localStorage
+  //   const token = localStorage.getItem('authToken');
+  //   if (token) {
+  //     setIsAuthenticated(true);
+  //   } else {
+  //     setIsAuthenticated(false);
+  //   }
+  // }, [isAuthenticated]);
 
   return (
     <Router>
       <Routes>
         {/* Redirect to login if not authenticated */}
-        <Route
+        {/* <Route
           path="/"
           element={isAuthenticated ? <TaskManagementBoard /> : <Navigate to="/login" />}
-        />
+        /> */}
         
         {/* Public routes */}
+        <Route path="/" element={<TaskManagementBoard />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         
